728a6649c998afe070dca2a6aac6269fb9f6e2ef
TERAN-XMD <earnonline6k@gmail.com>
2025-08-10 15:47:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const qrRouter = require('./qr');
+const codeRouter = require('./pair');
+
+app.use('/server', qrRouter);
+app.use('/code', codeRouter);
+
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'pair.html'));
+});
+app.get('/qr', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'qr.html'));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'main.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star MALVIN-XD
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
